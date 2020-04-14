56d97679f4e0111f8db1e2fbeda8588dc94fae24
timlai <timlai99@outlook.com>
2020-04-14 12:04:51 +0800

    prerelease

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,66 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+// this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "helloworld" is now active!');
+	console.log('decorator sample is activated');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let timeout: NodeJS.Timer | undefined = undefined;
 
-		// Display a message box to the user
-		const message = 'Hello World VS Code!';
-		vscode.window.showInformationMessage(message);
+	const stlDecorationType = vscode.window.createTextEditorDecorationType({
+		backgroundColor: { id: 'stl.background' }
 	});
 
-	context.subscriptions.push(disposable);
+	let activeEditor = vscode.window.activeTextEditor;
+
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+		}
+		const regExElements = /<stl:[\w]+[^>]*>([\s\S]*?)<\/stl:[\w]+>/gm;
+		const regExEntities = /{stl\.[^{}]*}|{stl:[^{}]*}|{content\.[^{}]*}|{channel\.[^{}]*}|{comment\.[^{}]*}|{request\.[^{}]*}|{sql\.[^{}]*}|{user\.[^{}]*}|{navigation\.[^{}]*}|{photo\.[^{}]*}/ig;
+		const text = activeEditor.document.getText();
+		const stlOptions: vscode.DecorationOptions[] = [];
+		let match;
+		while (match = regExElements.exec(text)) {
+			const startPos = activeEditor.document.positionAt(match.index);
+			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+			const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Element **' + match[0] + '**' };
+			stlOptions.push(decoration);
+		}
+		while (match = regExEntities.exec(text)) {
+			const startPos = activeEditor.document.positionAt(match.index);
+			const endPos = activeEditor.document.positionAt(match.index + match[0].length);
+			const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: 'Entity **' + match[0] + '**' };
+			stlOptions.push(decoration);
+		}
+		activeEditor.setDecorations(stlDecorationType, stlOptions);
+	}
+
+	function triggerUpdateDecorations() {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+		timeout = setTimeout(updateDecorations, 500);
+	}
+
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+
+	vscode.window.onDidChangeActiveTextEditor(editor => {
+		activeEditor = editor;
+		if (editor) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
+	vscode.workspace.onDidChangeTextDocument(event => {
+		if (activeEditor && event.document === activeEditor.document) {
+			triggerUpdateDecorations();
+		}
+	}, null, context.subscriptions);
+
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
